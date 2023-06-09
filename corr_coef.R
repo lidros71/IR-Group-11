e5180f74b4f2c20b38c6949b00ca6ca64427ee7a
lidros71 <faizsamsudin71@gmail.com>
2023-06-09 18:03:25 +0800

    Update corr_coef.R

--- corr_coef.R
+++ corr_coef.R
@@ -2,4 +2,4 @@ p10_avg <- as.numeric(p10[51, ])
 p100_avg <- as.numeric(p100[51,])
 
 correlation <- cor(p10_avg, p100_avg)
-print(cor(p10_avg, p100_avg))
+print(paste("correlation coefficient: ", correlation))
